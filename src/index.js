324c89880bb574b313c40fc0c518da9d15401ca1
Akash Tiwari <akashtiwaari19bca014@gmail.com>
2024-08-11 20:54:24 +0530

    db

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 // require('dotenv').config({ path:""})
 import mongoose from "mongoose";
-import { databaseName } from "./constants";
+import { databaseName } from "./constants.js";
+import express from "express";
+const app = express();
+
 
 ;( async ()=>{
     try {
-        const connect = await mongoose.connect(process.env.mongodb_URL/databaseName)
+        const connect = await mongoose.connect(`${process.env.mongodb_URL}/${databaseName}?retryWrites=true&w=majority`);
+        app.on("error",(error)=>{
+        console.log(`Error: ${error}`);
+        throw(error);
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listening on port: ${process.env.PORT}`);
+        })
+        
     } catch (error) {
-        console.log("Database cannot be connected ");
+        console.log(`Database cannot be connected ${error}`);
     }
 
 })()
